2f98050564c07a3fe84c8919dd6e361a71fd2565
arvindh07 <arvindhganesh11@gmail.com>
2024-05-24 10:22:10 +0530

    added styles for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const Header = () => {
     return (
-        <View>
-            <Text>Header</Text>
+        <View style={styles.headerContainer}>
+            <Text style={styles.text}>Todo - Get it done TODAY🚀</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        backgroundColor: "brown",
+        borderColor: "black",
+        borderRadius: 12
+    },
+    text: {
+        color: "white",
+        fontSize: 20,
+        textAlign: "center",
+        paddingVertical: 10,
+        paddingHorizontal: 12
+    }
+})
 
 export default Header;
