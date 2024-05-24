2ca06e137d28a8f003900654dadda7c91016fbe6
arvindh07 <arvindhganesh11@gmail.com>
2024-05-24 10:34:19 +0530

    display todo list with text comp

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const Body = () => {
+    const [todoList, setTodoList] = useState([
+        {key: 1, text: "Buy me a coffee"},
+        {key: 2, text: "Do exercise"},
+        {key: 3, text: "Complete Mern"}
+    ]);
+
     return (
         <View style={styles.bodyContainer}>
-            <Text>Body text</Text>
+            {/* <Text style={styles.text}>Body text</Text> */}
+            {todoList?.map((item) => (
+                <Text key={item?.key} style={styles.text}>{item?.text}</Text>
+            ))}
         </View>
     )
 }
@@ -13,9 +22,14 @@ const styles = StyleSheet.create({
     bodyContainer:{
         flex: 1,
         marginTop: 10,
-        backgroundColor: "crimson"
+        backgroundColor: "#0000001f",
+        padding: 10
     },
-
+    text: {
+        color: "black",
+        fontSize: 18,
+        fontFamily: "cursive"
+    }
 });
 
 export default Body;
